adc612b1b3b914ec0c4710274cd76f83888f6c26
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-08-14 09:23:44 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
 const getTodos = () => {
-  fetch(apiUrl + "?_limit=5")
+  fetch(apiUrl + "?_limit=10")
     .then((res) => res.json())
     .then((data) => {
       data.forEach((todo) => addTodoToDom(todo));
@@ -10,6 +10,7 @@ const getTodos = () => {
 
 const addTodoToDom = (todo) => {
   const div = document.createElement("div");
+  div.classList.add("todo");
   div.appendChild(document.createTextNode(todo.title));
   div.setAttribute("data-id", todo.id);
 
@@ -39,9 +40,42 @@ const createTodo = (e) => {
     .then((data) => addTodoToDom(data));
 };
 
+const toggleCompleted = (e) => {
+  if (e.target.classList.contains("todo")) {
+    e.target.classList.toggle("done");
+
+    updateTodo(e.target.dataset.id, e.target.classList.contains("done"));
+  }
+};
+
+const updateTodo = (id, completed) => {
+  fetch(`${apiUrl}/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ completed }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+const deleteTodo = (e) => {
+  if (e.target.classList.contains("todo")) {
+    const id = e.target.dataset.id;
+    fetch(`${apiUrl}/${id}`, {
+      method: "DELETE",
+    })
+      .then((rest) => rest.json())
+      .then(() => e.target.remove());
+  }
+};
+
 const init = () => {
   document.addEventListener("DOMContentLoaded", getTodos);
   document.querySelector("#todo-form").addEventListener("submit", createTodo);
+  document
+    .querySelector("#todo-list")
+    .addEventListener("click", toggleCompleted);
+  document.querySelector("#todo-list").addEventListener("dblclick", deleteTodo);
 };
 
 init();
